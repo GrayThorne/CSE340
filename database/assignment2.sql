a49ec2b82fd983db2b85c521fa2a5d4b473f3093
GrayThorne <momasboysn@gmail.com>
2024-02-06 16:22:41 -0700

    Fixed inner join

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -5,7 +5,7 @@
 -- 1 Insert for Tony Start 
 INSERT INTO account(account_firstname, account_lastname, account_email, account_password)
 VALUES('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n')
-
+arting pgAdmin 4...
 -- 2 Update to Tony Stark
 UPDATE account SET account_type = 'Admin' WHERE account_id = 1
 
@@ -20,7 +20,8 @@ WHERE inv_id = 10
 -- 5 Inner join for the Sport category
 SELECT inv_make, inv_model, classification_name
 FROM inventory INNER JOIN	classification 
-ON inventory.classification_id = 2
+ON inventory.classification_id = classification.classification_id
+WHERE inventory.classification_id = 2
 
 -- 6 Update all records to have vehicles in middle of the file path
 UPDATE inventory
